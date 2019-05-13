a7bf67c1b2377e3ea0878e051830cfe33ceb18ac
IanSani <iansani259@gmail.com>
2019-05-13 06:00:01 +0300

    Added search results function to be able to search image by category

--- images/views.py
+++ images/views.py
@@ -18,3 +18,15 @@ def home(request):
         return render(request, 'all-images.html',{"name":name,"images":images,"cat":cat})
 
     return render(request,"all-images.html",{"images":images,"location":location,"category":category})
+
+def search_results(request):
+    if 'categories' in request.GET and request.GET['Category']:
+        search_images =request.GET,get("Category")
+        searched_images = Image.search_by_category(search_images)
+        message = f"{search_images}"
+
+        return render(request, 'search.html',{"message":message,"images":searched_images})
+
+    else:
+        message ="No image has been searched"
+        return render(request, 'search.html',{"message":message})
