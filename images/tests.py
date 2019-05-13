f7c886100a12d41c788b132bc3323de4c23eff8a
IanSani <iansani259@gmail.com>
2019-05-13 11:17:38 +0300

    Created a location class to check for location functionality

--- images/tests.py
+++ images/tests.py
@@ -25,3 +25,26 @@ class ImageTest(TestCase):
         self.drinks.save()
         self.drinks.name ="MoreDrinks"
         self.asserTrue(self.drinks.name == 'MoreDrinks')
+
+    def test_all_images(self):
+        self.drinks.save()
+        images = Image.all_images()
+        self.asserTrue(len(images) >0)
+
+    def test_search_by_category(self):
+        self.drinks.save()
+        location = Image.view_location(self.nairobi)
+        self.asserTrue(len(location) >0)
+
+    def test_view_category(self):
+        self.drinks.save()
+        location =Image.view_category(self.music)
+        self.asserTrue(len(Category) >0)
+
+class LocationTest(Test)
+    def setUp(self):
+        self.nairobi = Location(name='nairobi')
+
+    def test_instance(self):
+        self.nairobi.save()
+        self.asserTrue(isinstance(self.nairobi, Location))
